ba2244dad956d9f6fc3764ffa18f75d3d825e009
MHShihan <mahmodul.no.1@gmail.com>
2024-06-16 03:02:52 +0600

    solved pattern 18

---
+++ patterns/pattern18.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a value: ";
+    cin >> n;
+
+    int row = 1;
+
+    while (row <= n)
+    {
+        // int col = 1, space = 1;
+        int col = 1, space = n - row;
+        while (space > 0)
+        {
+            cout << " ";
+            space--;
+        }
+
+        while (col <= row)
+        {
+            cout << row;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
