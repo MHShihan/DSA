544553275cd5730c1c76b36c91517fd5e1ddde43
MHShihan <mahmodul.no.1@gmail.com>
2024-07-03 18:55:08 +0600

    Slved swap alternate problem

---
+++ Array/swap-alternate.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int size)
+{
+    for (int i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            // swap(arr[i], arr[i + 1]);
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    int even[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int odd[5] = {1, 2, 3, 4, 5};
+
+    swapAlternate(even, 8);
+    printArray(even, 8);
+
+    swapAlternate(odd, 5);
+    printArray(odd, 5);
+
+    return 0;
+}
