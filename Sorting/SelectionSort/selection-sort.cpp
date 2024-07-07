5f16dfb60189b17c8241bdcf81f48b0e1bd9cf2c
MHShihan <mahmodul.no.1@gmail.com>
2024-07-07 12:09:34 +0600

    Learned selection sort

---
+++ Sorting/SelectionSort/selection-sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+}
+
+void selectionSort2(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void printingArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[5] = {64, 25, 12, 22, 11};
+    selectionSort2(arr, 5);
+    printingArray(arr, 5);
+
+    return 0;
+}
