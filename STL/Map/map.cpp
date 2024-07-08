4c7de2fe3ba0a53f0f5ae9a818abbf3f647a3404
MHShihan <mahmodul.no.1@gmail.com>
2024-07-08 16:44:14 +0600

    Learned Map in c++

---
+++ STL/Map/map.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<int, string> m;
+    m[1] = "Mahmudul";
+    m[10] = "Shihan";
+    m[2] = "Hasan";
+
+    m.insert({5, "MHS"});
+
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
+
+    cout << "Finding 10 -> " << m.count(10) << endl;
+
+    m.erase(10);
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl;
+    }
+
+    return 0;
+}
