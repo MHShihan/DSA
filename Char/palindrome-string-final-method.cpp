ca52ef1aa47e10d3dee41ff61b74f3f917d7fbcf
MHShihan <mahmodul.no.1@gmail.com>
2024-07-11 15:22:53 +0600

    Solved pelindrome string in more efficient way

---
+++ Char/palindrome-string-final-method.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+bool isAlphabetic(char ch)
+{
+    return ch >= '0' && ch <= '9' ||
+           ch >= 'A' && ch <= 'Z' ||
+           ch >= 'a' && ch <= 'z';
+}
+
+char toLowerCase(char ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        return ch - 'A' + 'a';
+    }
+    return ch;
+}
+
+bool isPalindrome(string s)
+{
+    int st = 0;
+    int e = s.size() - 1;
+
+    while (st <= e)
+    {
+        while (st < e && !isAlphabetic(s[st]))
+        {
+            st++;
+        }
+
+        while (st < e && !isAlphabetic(s[e]))
+        {
+            e--;
+        }
+
+        if (toLowerCase(s[st]) != toLowerCase(s[e]))
+        {
+            return 0;
+        }
+
+        st++;
+        e--;
+    }
+    return 1;
+}
+
+int main()
+{
+    string s = "";
+
+    cout << isPalindrome(s) << endl;
+
+    return 0;
+}
