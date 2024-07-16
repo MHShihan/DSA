21c150372109b8d46b8a79d9a742ec24235198dd
MHShihan <mahmodul.no.1@gmail.com>
2024-07-16 17:27:53 +0600

    Write function for col sum and rol sum in 2d Array

--- 2D Array/row-wise-sum.cpp
+++ 2D Array/row-wise-sum.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void getRowSum(int arr[][3], int row, int col)
+void printColSum(int arr[][3], int row, int col)
 {
-    cout << "Printing Sum -> " << endl;
+    cout << "Printing col Sum -> " << endl;
+    for (int i = 0; i < col; i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < row; j++)
+        {
+            sum += arr[j][i];
+        }
+
+        cout << sum << " ";
+    }
+
+    cout << endl;
+}
+
+void PrintRowSum(int arr[][3], int row, int col)
+{
+    cout << "Printing row Sum -> " << endl;
     for (int i = 0; i < row; i++)
     {
         int sum = 0;
@@ -31,7 +49,8 @@ int main()
         }
     }
 
-    getRowSum(arr, 3, 3);
+    PrintRowSum(arr, 3, 3);
+    printColSum(arr, 3, 3);
 
     return 0;
 }
