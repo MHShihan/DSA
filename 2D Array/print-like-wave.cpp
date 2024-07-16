ff4881a012c631331d2faa08df96f2f8a14c33b6
MHShihan <mahmodul.no.1@gmail.com>
2024-07-16 20:15:09 +0600

    Solved print like a wave problem from code 360

---
+++ 2D Array/print-like-wave.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> newArr;
+    for (int i = 0; i < mCols; i++)
+    {
+
+        if (i & 1)
+        {
+
+            for (int j = nRows - 1; j >= 0; j--)
+            {
+                newArr.push_back(arr[j][i]);
+            }
+        }
+        else
+        {
+            for (int j = 0; j < nRows; j++)
+            {
+                newArr.push_back(arr[j][i]);
+            }
+        }
+    }
+
+    return newArr;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}, {4, 44, 444, 4444}};
+
+    vector<int> ans = wavePrint(arr, 4, 4);
+
+    for (int i : ans)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
