a401e95af171f52e04c8f10ff7c9483b20873d07
MHShihan <mahmodul.no.1@gmail.com>
2024-07-27 12:54:58 +0600

    Solved factors of a number in more optimizing way

--- Math For DSA/factors-of-a-number.cpp
+++ Math For DSA/factors-of-a-number.cpp
@@ -10,6 +10,26 @@ void factors(int n)
             cout << i << " ";
         }
     }
+    cout << endl;
+}
+
+// More optimize way
+void factorsOfNumber(int n)
+{
+    for (int i = 1; i * i <= n; i++)
+    {
+
+        if (n % i == 0)
+        {
+            cout << i << " ";
+
+            if (i != n / i)
+            {
+                cout << n / i << " ";
+            }
+        }
+    }
+    cout << endl;
 }
 
 int main()
@@ -19,6 +39,7 @@ int main()
     cin >> n;
 
     factors(n);
+    factorsOfNumber(n);
 
     return 0;
 }
