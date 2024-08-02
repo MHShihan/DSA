673ff186f5df6670992435589a536489850834ef
MHShihan <mahmodul.no.1@gmail.com>
2024-08-02 14:10:06 +0600

    Learned gcd

---
+++ Math For DSA/gcd.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+
+    if (a == 0)
+        return b;
+
+    if (b == 0)
+        return a;
+
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+
+        if (b > a)
+            b = b - a;
+    }
+    return a;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter two number: ";
+    cin >> a >> b;
+
+    cout << "The GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+
+    return 0;
+}
