dfebc7d6c6fb18e7f04d314eb37a6ab94fedc052
MHShihan <mahmodul.no.1@gmail.com>
2024-08-12 13:10:27 +0600

    write another function in GCD

--- Math For DSA/gcd.cpp
+++ Math For DSA/gcd.cpp
@@ -21,6 +21,25 @@ int gcd(int a, int b)
     return a;
 }
 
+int gcd1(int a, int b)
+{
+
+    if (a == 0)
+        return b;
+
+    if (b == 0)
+        return a;
+
+    while (b != 0)
+    {
+        int rem = a % b;
+
+        a = b;
+        b = rem;
+    }
+    return a;
+}
+
 int main()
 {
     int a, b;
@@ -28,6 +47,7 @@ int main()
     cin >> a >> b;
 
     cout << "The GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+    cout << "The GCD of " << a << " and " << b << " is " << gcd1(a, b) << endl;
 
     return 0;
 }
