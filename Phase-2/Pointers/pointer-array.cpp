440ac634264308188143751f5bb870a884b2d6a5
MHShihan <mahmodul.no.1@gmail.com>
2024-08-15 11:17:14 +0600

    Learned difference between pointer and array

--- Phase-2/Pointers/pointer-array.cpp
+++ Phase-2/Pointers/pointer-array.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {2, 5, 7};
+    int arr[10] = {22, 41, 75, 101};
 
     cout << "Address of first memory block is " << arr << endl;
     cout << "Value at 0th index is " << arr[0] << endl;
@@ -13,6 +13,22 @@ int main()
     cout << "5th " << *arr + 1 << endl;
     cout << "6th " << *(arr + 1) << endl;
     cout << "7th " << *(arr) + 1 << endl;
+    cout << "8th " << arr[2] << endl;
+    cout << "9th " << *(arr + 2) << endl;
+
+    int i = 3;
+
+    cout << arr[i] << " " << *(arr + i) << " " << i[arr] << " " << *(i + arr) << endl;
+
+    int temp[10];
+    cout << sizeof(temp) << endl;
+    cout << "Temp1 " << sizeof(*temp) << endl;
+    cout << "Temp2 " << sizeof(&temp) << endl;
+
+    int *ptr = &temp[0];
+    cout << sizeof(ptr) << endl;
+    cout << sizeof(*ptr) << endl;
+    cout << sizeof(&ptr) << endl;
 
     return 0;
 }
