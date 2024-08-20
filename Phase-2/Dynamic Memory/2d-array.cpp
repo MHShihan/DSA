0b61e1dc43e326045d59d9b00fa241c23c625bc6
MHShihan <mahmodul.no.1@gmail.com>
2024-08-20 22:33:25 +0600

    Dynamic memory allocation for 2d array

---
+++ Phase-2/Dynamic Memory/2d-array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout << "Enter row: ";
+    cin >> row;
+
+    int col;
+    cout << "Enter col: ";
+    cin >> col;
+
+    int **arr = new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        arr[i] = new int[col];
+    }
+
+    // taking input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    // output
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
