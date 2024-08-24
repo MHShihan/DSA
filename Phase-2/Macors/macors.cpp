ea05ab78f3c7d632eedd06fac9f1291718d833c0
MHShihan <mahmodul.no.1@gmail.com>
2024-08-24 19:49:13 +0600

    create a macro function

--- Phase-2/Macors/macors.cpp
+++ Phase-2/Macors/macors.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 #define PI 3.1416
 
+#define SQUARE(n) (n * n)
+
 int main()
 {
     int r = 5;
 
+    int n = 7;
+    int result = SQUARE(n);
+
     double area = PI * r * r;
 
     cout << area << endl;
+    cout << "Square of " << n << " is " << result << endl;
 
     return 0;
 }
