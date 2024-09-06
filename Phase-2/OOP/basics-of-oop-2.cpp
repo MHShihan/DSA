1c021dd1932a88c1e70df260e2ef1de6c33777a6
MHShihan <mahmodul.no.1@gmail.com>
2024-09-07 01:57:44 +0600

    Learned parameterized constructer

---
+++ Phase-2/OOP/basics-of-oop-2.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+// Hero class
+class Hero
+{
+    // Properties
+private:
+    char level = 'x';
+
+public:
+    int health = 100;
+
+    // Creating constructor
+    Hero()
+    {
+        cout << "Constructor is called" << endl;
+    }
+
+    // Getter method
+    char getLevel()
+    {
+        return level;
+    }
+    // Setter method
+    void setLevel(char ch)
+    {
+        level = ch;
+    }
+};
+
+// Person class
+class Person
+{
+private:
+    int age;
+
+public:
+    string name;
+    char sex;
+
+    // Parameterized Constructor
+    Person(string name, int age, char sex)
+    {
+        this->age = age;
+        this->name = name;
+        this->sex = sex;
+    }
+
+    // Getter method
+    int getAge()
+    {
+        return age;
+    }
+
+    // Setter method
+    void setAge(int age)
+    {
+        this->age = age;
+    }
+};
+
+int main()
+{
+    // Static Allocation
+    Person p("Shihan", 23, 'M');
+    p.setAge(25);
+
+    cout << p.name << endl;
+    cout << p.getAge() << endl;
+    cout << p.sex << endl;
+
+    return 0;
+}
