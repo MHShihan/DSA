7fcc8d6b4a428bb2f8a3844450d59a9faf3d5df5
MHShihan <mahmodul.no.1@gmail.com>
2024-09-15 12:08:46 +0600

    Learned how to conver a array into linked list

--- Phase-2/Linked List/Linked List & Its Types/singly-linked-list.cpp
+++ Phase-2/Linked List/Linked List & Its Types/singly-linked-list.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
@@ -7,20 +7,43 @@ public:
     int data;
     Node *next;
 
+public:
     // Constructor
     Node(int data)
     {
         this->data = data;
-        this->next = NULL;
+        this->next = nullptr;
+    }
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
     }
 };
 
+Node *convertArr2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp->next;
+        mover = temp;
+    }
+
+    return head;
+}
+
 int main()
 {
-    Node *node1 = new Node(10);
+    vector<int> arr = {112, 5, 8, 9};
 
-    cout << node1->data << endl;
-    cout << node1->next << endl;
+    Node *head = convertArr2LL(arr);
+
+    cout << head->data << endl;
 
     return 0;
 }
