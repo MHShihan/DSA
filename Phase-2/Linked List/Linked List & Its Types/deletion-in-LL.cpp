e9ae7ee8d1d51c1e02bd07dedd40501bd2fec6f1
MHShihan <mahmodul.no.1@gmail.com>
2024-09-19 23:54:53 +0600

    Learn how to remove head from LL

---
+++ Phase-2/Linked List/Linked List & Its Types/deletion-in-LL.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *convertArray2LL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *removeHead(Node *head)
+{
+    Node *temp = head;
+    head = head->next;
+    delete temp;
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr = {2, 4, 6, 8};
+    Node *head = convertArray2LL(arr);
+    head = removeHead(head);
+    cout << head->data << endl;
+    printLL(head);
+    return 0;
+}
