9f6195de9d638c00fd1e1cfe1e2d29292a6cf15f
MHShihan <mahmodul.no.1@gmail.com>
2024-09-21 21:12:26 +0600

    Learn how to remove desired element from LL

--- Phase-2/Linked List/Linked List & Its Types/deletion-in-LL.cpp
+++ Phase-2/Linked List/Linked List & Its Types/deletion-in-LL.cpp
@@ -99,6 +99,35 @@ Node *removeK(Node *head, int k)
     return head;
 }
 
+Node *removeElement(Node *head, int element)
+{
+    if (head == NULL)
+        return head;
+
+    if (head->data == element)
+    {
+        Node *temp = head;
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    Node *temp = head;
+    Node *prev = NULL;
+    while (temp)
+    {
+        if (temp->data == element)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
 int main()
 {
     vector<int> arr = {2, 4, 6, 8};
@@ -108,7 +137,8 @@ int main()
     // cout << head->data << endl;
 
     // head = removeTail(head);
-    head = removeK(head, 10);
+    // head = removeK(head, 10);
+    head = removeElement(head, 6);
     printLL(head);
     return 0;
 }
