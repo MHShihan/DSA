e48518490c7080d55045fdd1eb08c6a7e845bf71
MHShihan <mahmodul.no.1@gmail.com>
2024-09-27 23:16:36 +0600

    Slolve reverse DLL using brute force apprach

--- Phase-2/Linked List/Linked List Questions/reverse-DLL.cpp
+++ Phase-2/Linked List/Linked List Questions/reverse-DLL.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
 class Node
@@ -26,6 +28,29 @@ public:
     }
 };
 
+Node *reverseDLLBruteForce(Node *head)
+{
+    stack<int> st;
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        st.push(temp->data);
+        temp = temp->next;
+    }
+
+    temp = head;
+
+    while (temp != NULL)
+    {
+        temp->data = st.top();
+        st.pop();
+        temp = temp->next;
+    }
+
+    return head;
+}
+
 Node *reverseDLL(Node *head)
 {
     if (head == NULL || head->next == NULL)
@@ -46,4 +71,39 @@ Node *reverseDLL(Node *head)
         current = current->prev;
     }
     return prev->prev;
+}
+
+Node *convertArrayDLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void printDLL(Node *head, vector<int> &arr)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {2, 4, 6, 8};
+    Node *head = convertArrayDLL(arr);
+    head = reverseDLLBruteForce(head);
+    head = reverseDLL(head);
+    printDLL(head, arr);
+    return 0;
 }
