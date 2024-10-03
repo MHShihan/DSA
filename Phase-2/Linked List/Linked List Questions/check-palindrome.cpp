6fab1bce6502aa356d00540a6df58282f7d67ce1
MHShihan <mahmodul.no.1@gmail.com>
2024-10-03 17:26:52 +0600

    Solve check palindrome problem by optimal way in Singly LL

--- Phase-2/Linked List/Linked List Questions/check-palindrome.cpp
+++ Phase-2/Linked List/Linked List Questions/check-palindrome.cpp
@@ -50,6 +50,50 @@ bool isPalindromeBruteForceApproach(Node *head)
     return true;
 }
 
+Node *reverseLinkedList(Node *head)
+{
+    // base case
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    Node *newHead = reverseLinkedList(head->next);
+
+    Node *front = head->next;
+    front->next = head;
+    head->next = NULL;
+
+    return newHead;
+}
+
+bool isPalindrome(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    Node *newHead = reverseLinkedList(slow->next);
+
+    Node *first = head;
+    Node *second = newHead;
+    while (second != NULL)
+    {
+        if (first->data != second->data)
+        {
+            reverseLinkedList(newHead);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+
+    reverseLinkedList(newHead);
+    return true;
+}
+
 int main()
 {
 
