98d694229683c8acd74b9214a7cc5cb071a1de64
MHShihan <mahmodul.no.1@gmail.com>
2024-10-04 09:47:16 +0600

    Solved problem middle of Linked list

---
+++ Phase-2/Linked List/Linked List Questions/middle-of-LL.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *findMiddle(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
